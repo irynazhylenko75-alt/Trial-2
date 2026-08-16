8bd3cb3a0adcd7aecb7008b2660b65145b023c50
irynazhylenko75-alt <irynazhylenko75@gmail.com>
2026-08-16 21:19:59 +0300

    second commit

---
+++ Lesson01/fake hm_01.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a number: "))
+print(a + 10)
+# int a;
+# cout<<"Enter number: ";
+# cin>>a;
+# print(a+10);
+a = float(input ("Enter a number"))
+print (a + 155)
+
+#int, float, double, string
