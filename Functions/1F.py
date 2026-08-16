c913d4a200cf09ed519139a4ce6fa78c7c23a5be
irynazhylenko75-alt <irynazhylenko75@gmail.com>
2026-08-16 20:01:24 +0300

    changed 60 with 25

---
+++ Functions/1F.py
@@ -0,0 +1,14 @@
+a = 7
+
+print(a)
+
+b = a
+print(id(a))
+print(id(b))
+
+a = "liberty"
+
+print(id(a))
+
+b = b + 2
+print(id(b))
--- Lesson01/01.py
+++ Lesson01/01.py
@@ -1,1 +1,11 @@
-print ("Hello World")
+age = 10
+b = "world"
+c = age
+user_name = "Bob"
+
+print(a)
+print(b)
+print(c)
+
+age = "new text"
+print(a)
