ac48b68e4582c3c9c2ea3392be9c624ff87ab4af
irynazhylenko75-alt <irynazhylenko75@gmail.com>
2026-09-13 20:19:38 +0300

    second commit

---
+++ Lesson01/ДЗ 8.2. Паліандром.py
@@ -0,0 +1,13 @@
+def is_palindrome(text):
+    cleaned_text = "".join(char.lower() for char in text if char.isalnum())
+    reversed_text = "".join(reversed(cleaned_text))
+    if cleaned_text == reversed_text:
+        return True
+    else:
+        return False
+
+assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+assert is_palindrome('0P') == False, 'Test2'
+assert is_palindrome('a.') == True, 'Test3'
+assert is_palindrome('aurora') == False, 'Test4'
+print("ОК")
